a7bb7508999f70dff827cbd3fc40030037fd0a6b
Noah Greff <greffnoah@gmail.com>
2023-02-14 01:30:18 +0100

    added `CommandApi`

---
+++ src/main/java/net/azalealibrary/command/CommandApi.java
@@ -0,0 +1,21 @@
+package net.azalealibrary.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.Field;
+
+public final class CommandApi {
+
+    public static void register(Plugin plugin, Class<? extends CommandNode> command) {
+        try {
+            Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+            CommandMap map = (CommandMap) field.get(Bukkit.getServer());
+            map.register(plugin.getName(), command.getConstructor().newInstance());
+        } catch (Exception exception) {
+            throw new AzaleaException("An error occurred while registering command.", exception.getMessage());
+        }
+    }
+}
